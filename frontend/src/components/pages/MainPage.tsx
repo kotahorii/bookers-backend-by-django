c8c895cff144b25c09176c5bb9d21e256371b87b
kotahorii <khorii.micron@gmail.com>
2021-10-22 00:15:21 +0900

    authentication by useEffect

--- frontend/src/components/pages/MainPage.tsx
+++ frontend/src/components/pages/MainPage.tsx
@@ -1,10 +1,17 @@
-import { VFC } from "react";
+import { VFC, useEffect } from "react";
 import { Header } from "../organisms/Header";
 import { Container, Flex } from "@chakra-ui/layout";
-import { Outlet } from "react-router";
+import { Outlet, useNavigate } from "react-router";
 import { SideBar } from "../organisms/SideBar";
 
 export const MainPage: VFC = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("localJWT");
+    !token && navigate("login/");
+  }, []);
+
   return (
     <>
       <Flex minH="100vh" bg="gray.50" direction="column">
